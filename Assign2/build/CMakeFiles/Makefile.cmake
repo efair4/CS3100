048353943976800d3846ce2d672c3a642e97aa0e
Ember Fairbanks <ember.fairbanks@aggiemail.usu.edu>
2017-09-14 19:52:10 -0600

    Modify CMakeLists.txt

--- Assign2/build/CMakeFiles/Makefile.cmake
+++ Assign2/build/CMakeFiles/Makefile.cmake
@@ -111,5 +111,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/Assign2.dir/DependInfo.cmake"
+  "CMakeFiles/assign2.dir/DependInfo.cmake"
   )
---
+++ Assign2/build/CMakeFiles/assign2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assign2.dir/main.cpp.o"
+  "CMakeFiles/assign2.dir/compute-fib.cpp.o"
+  "CMakeFiles/assign2.dir/compute-e.cpp.o"
+  "CMakeFiles/assign2.dir/compute-pi.cpp.o"
+  "assign2.pdb"
+  "assign2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assign2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
